a9ee06231102b083e1bd5a4e16fb29a728c041bc
Tran Manh Lam <lam_tran@knorex.com>
2015-09-05 01:40:22 +0700

    Define model for our contact object

--- app/models/contact.server.model.js
+++ app/models/contact.server.model.js
@@ -10,12 +10,66 @@ var mongoose = require('mongoose'),
  * Contact Schema
  */
 var ContactSchema = new Schema({
-	name: {
+	firstName: {
 		type: String,
 		default: '',
-		required: 'Please fill Contact name',
+		required: 'Please fill Contact\'s first name',
 		trim: true
 	},
+	lastName: {
+		type: String,
+		default: '',
+		required: 'Please fill Contact\'s last name',
+		trim: true
+	},
+	email: {
+		type: String,
+		default: '',
+		require: 'Please fill in email',
+		trim: true
+	},
+	gender: {
+		type: String,
+		default: 'm',
+		enum: ['m', 'f', 'u'],
+		trim: true
+	},
+	phone: {
+		type: String,
+		required: 'Please input phone number',
+		trim: true
+	},
+	addresses: [
+		{
+			addressLine1: {
+				type: String,
+				default: '',
+				required: 'Please input address line 1',
+				trim: true,
+			},
+			addressLine2: {
+				type: String,
+				default: '',
+				required: 'Please input addresss line 2',
+				trim: true,
+			},
+			phone: {
+				type: String,
+				default: '',
+				trim: true
+			},
+			postalCode: {
+				type: String,
+				default: '',
+				required: 'Please input postal code',
+				trim: true
+			},
+			isActive: {
+				type: Boolean,
+				default: false,
+			}
+		}
+	],
 	created: {
 		type: Date,
 		default: Date.now
